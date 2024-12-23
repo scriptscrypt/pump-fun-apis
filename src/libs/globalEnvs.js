18245b52a768fce275a78edee83df22a0814eec3
Scriptscrypt <tirumaladasa@gmail.com>
2024-12-23 21:42:19 +0530

    fix: libs folder 2

---
+++ src/libs/globalEnvs.js
@@ -0,0 +1,4 @@
+envSOLANA_RPC_URL = process.env.SOLANA_RPC_URL
+envWALLET_PRIVATE_KEY = process.env.WALLET_PRIVATE_KEY
+envWALLET_PUBLIC_KEY = process.env.WALLET_PUBLIC_KEY
+envPORT =   process.env.PORT
